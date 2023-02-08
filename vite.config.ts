d1833f0ac84e0377a572d71de32e34123ecfc2ce
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-02-08 07:59:30 +0530

    Vite Config Formatted

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
-});
+})
