65288225b6d95c4d1f5268314e50589e18b0b612
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-02-23 19:56:36 +0530

    Vite Plugins Added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
+import { fileURLToPath, URL } from "url"
+
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
+import svgrPlugin from "vite-plugin-svgr"
+import viteTsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    svgrPlugin(),
+    viteTsconfigPaths(),
+    splitVendorChunkPlugin(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
