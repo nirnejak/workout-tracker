8c027eec025a990fd411ea5243b60ccd9346400f
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-28 21:32:37 +0530

    Deprecated vite config removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from "url"
 
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig, splitVendorChunkPlugin } from "vite"
+import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
 import svgrPlugin from "vite-plugin-svgr"
 import viteTsconfigPaths from "vite-tsconfig-paths"
@@ -15,7 +15,6 @@ export default defineConfig({
     react(),
     svgrPlugin(),
     viteTsconfigPaths(),
-    splitVendorChunkPlugin(),
     VitePWA({ registerType: "autoUpdate" }),
   ],
   resolve: {
