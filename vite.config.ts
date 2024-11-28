b0a481ee41dbb31a0223bdf87e5141288ffbdb90
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-28 21:29:40 +0530

    development port as 3000

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import viteTsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     react(),
     svgrPlugin(),
