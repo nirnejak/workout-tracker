0573bbfa06d1892e1638f0b573e29ac14fde4ffb
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-13 14:28:14 +0530

    Home: classes fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,14 +8,13 @@ import WorkoutsProvider from "../store/WorkoutsContext"
 const App: React.FC = () => {
   const { theme } = useTheme()
   return (
-    <main className="bg-[--color-light]">
+    <main>
       <WorkoutsProvider>
         <Workouts />
         <CommandBar />
       </WorkoutsProvider>
-      <p className="fixed bottom-0 left-5 mb-5 hidden text-sm md:block">
-        <span className="text-[--color-dark]">Current Theme: </span>
-        <span className="text-[--color-dark]">{theme}</span>
+      <p className="fixed bottom-0 left-5 mb-5 hidden text-sm text-[--color-dark] md:block">
+        Current Theme: {theme}
       </p>
       <p className="fixed bottom-0 right-5 mb-5 hidden text-sm md:block">
         <a
