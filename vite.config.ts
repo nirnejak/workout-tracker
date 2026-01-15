f4b336a6038462aae2cf0e91e7fe5629b4d61508
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-15 13:56:35 +0530

    Vite with React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { fileURLToPath, URL } from "url"
 
-import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa"
 import svgrPlugin from "vite-plugin-svgr"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
     svgrPlugin(),
     viteTsconfigPaths(),
     VitePWA({ registerType: "autoUpdate" }),
