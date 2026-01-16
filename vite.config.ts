a5e3927c244521e62ee9c376dabdd668c74e8f9b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-16 06:18:05 +0530

    vite config updatd

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from "url"
-
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa"
@@ -20,9 +18,4 @@ export default defineConfig({
     viteTsconfigPaths(),
     VitePWA({ registerType: "autoUpdate" }),
   ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
 })
