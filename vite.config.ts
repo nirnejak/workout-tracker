7e157e6e129c9be927f27dc627071fb18e59be01
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-05-18 03:43:15 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
 import svgrPlugin from "vite-plugin-svgr"
 import viteTsconfigPaths from "vite-tsconfig-paths"
